03b727a7da45e66d5689ae46b4a13d86c569ff64
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2022-08-24 20:43:13 +0530

    added Procfile for deployment

--- myblog/settings.py
+++ myblog/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-43171#g(axwd2k^(%eb_hka4zbz65qp%$__v1kt-7n^5uxb_u&'
-
+#SECRET_KEY = 'django-insecure-43171#g(axwd2k^(%eb_hka4zbz65qp%$__v1kt-7n^5uxb_u&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoblogvicky.herokuapp.com']
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
