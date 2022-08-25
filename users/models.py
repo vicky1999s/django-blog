cdb4c2a8790635d80066b41f9965434abf9710a4
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2022-08-25 17:42:08 +0530

    deleted media

--- users/models.py
+++ users/models.py
@@ -11,8 +11,9 @@ class profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save()
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            new_size = (300, 300)
-            img.thumbnail(new_size)
-            img.save(self.image.path)
+        # print("--------"+self.image.path)
+        # img = Image.open(self.image.path)
+        # if img.height > 300 or img.width > 300:
+        #     new_size = (300, 300)
+        #     img.thumbnail(new_size)
+        #     img.save(self.image.path)
