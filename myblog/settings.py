73f14d1c958429c79ff8f9c2280680f1afab0dce
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2022-08-25 13:38:13 +0530

    set debug to true

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-43171#g(axwd2k^(%eb_hka4zbz65qp%$__v1kt-7n^5uxb_u&'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
+#DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['djangoblogvicky.herokuapp.com']
 
